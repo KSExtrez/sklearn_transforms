cb4fa49c963c57936b8e3fa9c907a40d548b340b
KSExtrez <45128933+KSExtrez@users.noreply.github.com>
2020-08-27 13:35:21 -0500

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -21,7 +21,7 @@ class DataFrameImputer(TransformerMixin):
     def fit(self, X, y=None):
 
         self.fill = pd.Series([X[c].value_counts().index[0]
-            if X[c].dtype == np.dtype('O') else X[c].mean() for c in X],
+            if X[c].dtype == np.dtype('O') else X[c].median() for c in X],
             index=X.columns)
 
         return self
